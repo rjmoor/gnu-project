60a0857c083aae0c89623f65b8669d51cb5d9aeb
PIAAR <PIAAR@rmasters.group>
2023-11-28 13:32:32 -0500

    First Commit
    Initial Files correction

--- spa1/src/App.js
+++ spa1/src/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
+import Navigation from './components/navigation/navigation';
+import Main from './components/views/main';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>SPA #1</h1>
+        <Navigation />
+        {/* <Logo /> */}
+        <Main />
+        {/* <ImageLinkForm />
+        <FaceRecognition />
+        <Footer /> */}
       </div>
     );
   }
--- spa1/src/components/navigation/navigation.js
+++ spa1/src/components/navigation/navigation.js
@@ -1,10 +1,25 @@
-import  React  from 'react';
+import React from 'react';
+import './Nav.css';
 
 const Navigation = () => {
     return (
-        <nav>
-            <p>Sign Out</p>
-        </nav>
+        <div class="Navigation flex mv3">
+            <div class="outline w-25 pa3 mr2">
+            <code>Here</code>
+            </div>
+            <div class="outline w-25 pa3 mr2">
+            <code>There</code>
+            </div>
+            <div class="outline w-25 pa3 mr2">
+            <code>Elsewhere</code>
+            </div>
+            <div class="outline w-25 pa3 mr2">
+            <code>Anywhere</code>
+            </div>
+            <div class="outline w-25 pa3">
+            <code>Nowhere</code>
+            </div>
+         </div>
     );
 }
 
