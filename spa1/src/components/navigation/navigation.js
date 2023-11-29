c12b57a5b0d5c1ed2e413ec2819e0c4d9dadb1a3
PIAAR <PIAAR@rmasters.group>
2023-11-29 07:25:12 -0500

    Card placement
    Four cards

--- spa1/src/components/navigation/navigation.js
+++ spa1/src/components/navigation/navigation.js
@@ -3,21 +3,21 @@ import './Nav.css';
 
 const Navigation = () => {
     return (
-        <div class="Navigation flex mv3">
-            <div class="outline w-25 pa3 mr2">
-            <code>Here</code>
+        <div className="Navigation flex mv3">
+            <div className="outline link w-25 pa1 mr2">
+            <p>Here</p>
             </div>
-            <div class="outline w-25 pa3 mr2">
-            <code>There</code>
+            <div class="outline w-25 pa1 mr2">
+            <p>There</p>
             </div>
-            <div class="outline w-25 pa3 mr2">
-            <code>Elsewhere</code>
+            <div class="outline w-25 pa1 mr2">
+            <p>Elsewhere</p>
             </div>
-            <div class="outline w-25 pa3 mr2">
-            <code>Anywhere</code>
+            <div class="outline w-25 pa1 mr2">
+            <p>Anywhere</p>
             </div>
-            <div class="outline w-25 pa3">
-            <code>Nowhere</code>
+            <div class="outline w-25 pa1 mr2">
+            <p>Nowhere</p>
             </div>
          </div>
     );
--- spa1/src/components/views/main.js
+++ spa1/src/components/views/main.js
@@ -7,27 +7,33 @@ const Main = () => {
             <div className='pa1 pa1-ns'>
                 <h1>I guess this page is for my new content.</h1>
             </div>
-            <div className='pa1 pa1-ns'>
-                <h2> Gifts </h2>
-                <p className='Main fl'>
+            <div className='fl w-50'>
+                <h2 className='card-title'> Gifts </h2>
+                <p className='Main'>
                     We at Renaissance Masters Group would like to take this opportunity to share some key information with our customers about our services. We are excited to announce that we regularly offer specials and promotions, which are especially beneficial for those interested in attending our upcoming workshops. Additionally, we provide discounts on our exclusive merchandise and Notion templates, designed to enhance your personal development journey.
                     To stay updated on these offers and to gain access to a wealth of resources and insights, we encourage everyone to connect with us on our social media platforms. You can find us on Instagram, Facebook, and Twitter, where we share regular updates, inspirational content, and details about our services. Moreover, subscribing to our YouTube channel is a great way to stay informed about our latest workshops, promotions, and to get a glimpse of the transformative experiences we offer.
                     Our social media channels and YouTube are not just platforms for updates; they are vibrant communities where we engage with our audience, share valuable tips, and provide a sneak peek into the impactful work we do at Renaissance Masters Group. We invite you to join our growing community, participate in our conversations, and take the first step towards a more fulfilling personal and professional life with us.
                 </p>
-                <h2> Templates </h2>
-                <p className='Main fr'>
+            </div>
+            <div className='fl w-50'>
+                <h2 className='card-title'> Templates </h2>
+                <p className='Main'>
                     We at Renaissance Masters Group would like to take this opportunity to share some key information with our customers about our services. We are excited to announce that we regularly offer specials and promotions, which are especially beneficial for those interested in attending our upcoming workshops. Additionally, we provide discounts on our exclusive merchandise and Notion templates, designed to enhance your personal development journey.
                     To stay updated on these offers and to gain access to a wealth of resources and insights, we encourage everyone to connect with us on our social media platforms. You can find us on Instagram, Facebook, and Twitter, where we share regular updates, inspirational content, and details about our services. Moreover, subscribing to our YouTube channel is a great way to stay informed about our latest workshops, promotions, and to get a glimpse of the transformative experiences we offer.
                     Our social media channels and YouTube are not just platforms for updates; they are vibrant communities where we engage with our audience, share valuable tips, and provide a sneak peek into the impactful work we do at Renaissance Masters Group. We invite you to join our growing community, participate in our conversations, and take the first step towards a more fulfilling personal and professional life with us.
                 </p>
-                <h2> Workshops </h2>
-                <p className='Main fl'>
+            </div>
+            <div className='fl w-50'>
+                <h2 className='card-title'> Workshops </h2>
+                <p className='Main'>
                     We at Renaissance Masters Group would like to take this opportunity to share some key information with our customers about our services. We are excited to announce that we regularly offer specials and promotions, which are especially beneficial for those interested in attending our upcoming workshops. Additionally, we provide discounts on our exclusive merchandise and Notion templates, designed to enhance your personal development journey.
                     To stay updated on these offers and to gain access to a wealth of resources and insights, we encourage everyone to connect with us on our social media platforms. You can find us on Instagram, Facebook, and Twitter, where we share regular updates, inspirational content, and details about our services. Moreover, subscribing to our YouTube channel is a great way to stay informed about our latest workshops, promotions, and to get a glimpse of the transformative experiences we offer.
                     Our social media channels and YouTube are not just platforms for updates; they are vibrant communities where we engage with our audience, share valuable tips, and provide a sneak peek into the impactful work we do at Renaissance Masters Group. We invite you to join our growing community, participate in our conversations, and take the first step towards a more fulfilling personal and professional life with us.
                 </p>
-                <h2> Courses </h2>
-                <p className='Main fr'>
+            </div>
+            <div className='fl w-50'>
+                <h2 className='card-title'> Courses </h2>
+                <p className='Main'>
                     We at Renaissance Masters Group would like to take this opportunity to share some key information with our customers about our services. We are excited to announce that we regularly offer specials and promotions, which are especially beneficial for those interested in attending our upcoming workshops. Additionally, we provide discounts on our exclusive merchandise and Notion templates, designed to enhance your personal development journey.
                     To stay updated on these offers and to gain access to a wealth of resources and insights, we encourage everyone to connect with us on our social media platforms. You can find us on Instagram, Facebook, and Twitter, where we share regular updates, inspirational content, and details about our services. Moreover, subscribing to our YouTube channel is a great way to stay informed about our latest workshops, promotions, and to get a glimpse of the transformative experiences we offer.
                     Our social media channels and YouTube are not just platforms for updates; they are vibrant communities where we engage with our audience, share valuable tips, and provide a sneak peek into the impactful work we do at Renaissance Masters Group. We invite you to join our growing community, participate in our conversations, and take the first step towards a more fulfilling personal and professional life with us.
